9e39a765b731f748a3abe143cbf180de73b1a64c
hz810 <140937591+hz810@users.noreply.github.com>
2026-09-17 19:31:46 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,23 @@
+var adConfig = {
+    title: '📢 官方内置广告 · 优质推荐',
+    blockDomains: [
+        'example.com',
+        'test.com',
+    ],
+    ads: [
+        { text: '广告一', url: 'https://www.a.com', icon: '' },
+        { text: '广告二', url: 'https://www.b.com', icon: '' },
+        { text: '广告三', url: 'https://www.c.com', icon: '' },
+        { text: '广告四', url: 'https://www.d.com', icon: '' },
+        { text: '广告五', url: 'https://www.e.com', icon: '' },
+        { text: '广告六', url: 'https://www.f.com', icon: '' },
+        /*/
+        { text: '广告七', url: 'https://www.g.com', icon: '' },
+        { text: '广告八', url: 'https://www.h.com', icon: '' },
+        { text: '广告九', url: 'https://www.i.com', icon: '' },
+        { text: '广告十', url: 'https://www.j.com', icon: '' },
+        { text: '广告十一', url: 'https://www.k.com', icon: '' },
+        { text: '广告十二', url: 'https://www.l.com', icon: '' },
+        /*/
+    ]
+};
